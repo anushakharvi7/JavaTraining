afeaa5201ba68bf5c2cdbc28f9a2096cd2d06bac
Anusha Shivraj Kharvi <anushakharvi015@gmail.com>
2026-01-29 22:49:02 +0530

    new commit

---
+++ src/print/Start.java
@@ -0,0 +1,12 @@
+package print;
+
+import jdk.internal.org.jline.terminal.TerminalBuilder.SystemOutput;
+
+public class Start {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
