17b8e0d4a04d6e380b66f58d7116927e36c26b28
Anusha Shivraj Kharvi <anushakharvi015@gmail.com>
2026-02-10 21:44:37 +0530

    add file

---
+++ src/collections/WishList.java
@@ -0,0 +1,24 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class WishList {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> wishlist=new ArrayList<String>();
+		wishlist.add("Have Money");
+		wishlist.add("Buy a House");
+		wishlist.add("Buy a BMW Car");
+		wishlist.add("Travel the world");
+		wishlist.add("");
+		wishlist.add("");
+		wishlist.add("");
+		wishlist.add("");
+		wishlist.add("");
+		wishlist.add("");
+		
+		
+	}
+
+}
